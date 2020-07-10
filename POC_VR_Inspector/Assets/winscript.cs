2e7e66f99077bb02eeac1c05739cedbb7f0efcd4
Aurélien Ursulet <aurelien.ursulet@gmail.com>
2020-07-10 12:34:09 +0200

    POC

---
+++ POC_VR_Inspector/Assets/winscript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winscript : MonoBehaviour
+{
+    public void Youwin()
+    {
+        GameManager.instance.Win();
+    }
+    
+}
