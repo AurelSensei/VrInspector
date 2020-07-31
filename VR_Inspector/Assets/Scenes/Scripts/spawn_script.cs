1ce75ff495cf01d961349f7014080b19ca5c694b
SirMewtwo <mathis1.pellerin@epitech.eu>
2020-07-31 03:08:51 +0200

    menu from character test

---
+++ VR_Inspector/Assets/Scenes/Scripts/spawn_script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_script : MonoBehaviour
+{
+
+    public Panel currentPanel = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPanel.Hide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            currentPanel.Show();
+        }
+    }
+
+    public void canvasShow()
+    {
+
+    }
+
+    public void canvasButtonHide()
+    {
+        currentPanel.Hide();
+    }
+
+}
